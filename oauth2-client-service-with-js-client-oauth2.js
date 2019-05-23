7652fde8b66a0079261f8555d4853b2661615b1d
Shirou <nghinhut@gmail.com>
2019-05-23 16:05:51 +0700

    Add comments

--- oauth2-client-service-with-js-client-oauth2.js
+++ oauth2-client-service-with-js-client-oauth2.js
@@ -3,7 +3,7 @@ var ClientOAuth2 = require('client-oauth2'); // https://github.com/mulesoft/js-c
 var vdatOAuth2 = new ClientOAuth2({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  accessTokenUri: process.env.OAUTH2_SERVER_URL + '/oauth2/token',    
+  accessTokenUri: process.env.OAUTH2_SERVER_URL + '/oauth2/token',
   // authorizationUri: process.env.OAUTH2_SERVER_URL + '/oauth2/token',
   // redirectUri: 'http://example.com/auth/github/callback',
   scopes: ['profile']
@@ -19,8 +19,9 @@ getToken = () => {
         // console.log(ClientOAuth2Token); //=> { accessToken: '...', tokenType: 'bearer', ... }
         // console.log(ClientOAuth2Token.tokenType);
         // console.log(ClientOAuth2Token.accessToken)
-
+        
         resolve(ClientOAuth2Token.tokenType + ' ' + ClientOAuth2Token.accessToken);
+        // ex: bearer UtqialQW1vSqBGux1-GQjOh2QEnz3cQHCnUwV--DZVc.ZByXKuqFc2sbG8slKwRuCevLcmCb9LndzJMKipHu-zQ
       })
       .catch(reject)
   })
